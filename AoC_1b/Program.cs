59db41702ee0d7a0e56071f880e6eb84a3609330
gschandan <gsc128@gmail.com>
2021-12-01 23:41:43 +0000

    Solution part 1b

--- AoC_1b/Program.cs
+++ AoC_1b/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
-namespace AoC_1b
+using System.IO;
+using System.Collections.Generic;
+namespace AoC_1a
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputFile = File.ReadAllLines(@"./input.txt");
+            var inputText = new List<string>(inputFile);
+            List<int> inputNums = inputText.ConvertAll(str => Int32.Parse(str));
+            int currentMaxSum, currentSum, nextSum, incCounter = 0;
+            for (int i = 0, m = inputNums.Count-4; i <= m; i++)
+            {
+                currentSum = inputNums[i] + inputNums[i + 1] + inputNums[i + 2];
+                nextSum = inputNums[i+1] + inputNums[i + 2] + inputNums[i + 3];
+                if (nextSum > currentSum) incCounter++;
+                currentMaxSum = Math.Max(nextSum, currentSum);
+            }
+
+            Console.WriteLine(incCounter);
         }
     }
 }
