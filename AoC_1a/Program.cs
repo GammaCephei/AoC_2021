ee68c9e35991282180c6c77dc10fbae6fc19cf3b
gschandan <gsc128@gmail.com>
2021-12-01 22:45:38 +0000

    .

---
+++ AoC_1a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentLine = 0, currentMax = 0, increasedCounter  = 0;
+            foreach (var n in File.ReadLines(@"../../../input.txt"))
+            {
+                int currentNum = Int32.Parse(n);
+                if (currentLine++ == 0)
+                {
+                    currentMax = currentNum;
+                    continue;
+                }
+
+                if (currentNum > currentMax) increasedCounter++;
+                currentMax = currentNum;
+            }
+            Console.Write($"Increased Count:{increasedCounter}");
+        }
+    }
+}
