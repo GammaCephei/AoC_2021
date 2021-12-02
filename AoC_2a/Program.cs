612317aec9e2feb26dab995517c97eb4d2425474
gschandan <gsc128@gmail.com>
2021-12-02 19:32:00 +0000

    Solution 2a

---
+++ AoC_2a/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horiz = 0, vert = 0;
+            foreach (var line in File.ReadLines(@"../../../input.txt"))
+            {
+                var instructions = line.Split(" ");
+                var direction = instructions[0];
+                int amount = Int32.Parse(instructions[1]);
+                switch (direction)
+                {
+                    case "forward":
+                        horiz += amount;
+                        break;
+                    case "up":
+                        vert -= amount;
+                        break;
+                    case "down":
+                        vert += amount;
+                        break;
+                }
+                
+            }
+            Console.WriteLine($"{horiz}, {vert}, {horiz*vert}");
+        }
+    }
+}
